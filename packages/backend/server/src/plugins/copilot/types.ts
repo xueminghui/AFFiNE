81aa3f19e815cce71d537469997b7a0ee0d8b350
DarkSky <darksky2048@gmail.com>
2024-03-26 17:29:11 +0800

    feat: init resolver

--- packages/backend/server/src/plugins/copilot/types.ts
+++ packages/backend/server/src/plugins/copilot/types.ts
@@ -37,19 +37,6 @@ export function AvailableModelToTiktokenModel(
   }
 }
 
-export const CopilotSchema = z
-  .object({
-    // total used token of workspace
-    tokens: z.number(),
-    // count for chat of workspace
-    chatCount: z.number(),
-    // count for action of workspace
-    actionCount: z.number(),
-  })
-  .strict();
-
-export type Copilot = z.infer<typeof CopilotSchema>;
-
 // ======== ChatMessage ========
 
 export const ChatMessageSchema = z
@@ -64,6 +51,16 @@ export const ChatMessageSchema = z
   .strict();
 
 export type ChatMessage = z.infer<typeof ChatMessageSchema>;
+
+export const ChatHistorySchema = z
+  .object({
+    sessionId: z.string(),
+    tokens: z.number(),
+    messages: z.array(ChatMessageSchema),
+  })
+  .strict();
+
+export type ChatHistory = z.infer<typeof ChatHistorySchema>;
 
 // ======== Provider Interface ========
 
